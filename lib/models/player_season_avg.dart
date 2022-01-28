bdfab9d441de087a1a7f834138262e6b0d3b45ca
Michael Olie <mike@grolens.com>
2022-01-27 18:20:13 -0800

    added push script and initialized mobx

---
+++ lib/models/player_season_avg.dart
@@ -0,0 +1,48 @@
+// class SeasonAverage {
+//   int gamesPlayed;
+//   int playerId;
+//   double fgm;
+//   double fga;
+//   double fg3m;
+//   double ftm;
+//   double fta;
+//   double oreb;
+//   double dreb;
+//   double reb;
+//   double ast;
+//   double stl;
+//   double blk;
+//   double turnover;
+//   double pf;
+//   double fgPct;
+//   double fg3Pct;
+//   double ftPct;
+//   String image;
+  
+//   const SeasonAverage({
+//     required this.gamesPlayed,
+//     required this.playerId,
+//     required this.fgm,
+//     required this.fga,
+//     required this.fg3m,
+//     required this.ftm,
+//     required this.fta,
+//     required this.oreb,
+//     required this.dreb,
+//     required this.reb,
+//     required this.ast,
+//     required this.stl,
+//     required this.blk,
+//     required this.turnover,
+//     required this.pf,
+//     required this.fgPct,
+//     required this.fg3Pct,
+//     required this.ftPct,
+//     required this.image,
+//   })
+//   factory SeasonAverage.fromJson(Map<String, dynamic> json) {
+//     return SeasonAverage(
+      
+//     )
+//   }
+// };
