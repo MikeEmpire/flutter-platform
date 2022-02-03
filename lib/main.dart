bdfed488345ba51da3603f3e46f7ec83acfb87e1
Michael Olie <mike@grolens.com>
2022-02-03 12:01:22 -0800

    imported custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -17,17 +17,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Elite Mobile App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.amber,
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.amber,
+          fontFamily: "NeueHaas"),
       home: EliteHomePage(),
     );
   }
@@ -53,6 +53,8 @@ class EliteHomePage extends StatelessWidget {
           ),
           actions: const <Widget>[],
         ),
-        body: Center());
+        body: const Center(
+            child: Text("Search For Player by name",
+                style: TextStyle(fontFamily: "NeueHaas"))));
   }
 }
