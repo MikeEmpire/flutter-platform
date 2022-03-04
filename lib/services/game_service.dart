19dd2872c7cdf615dc57dbaaf7482c49f08c3324
Michael Olie <mike@grolens.com>
2022-03-04 07:51:45 -0800

    setup game service to retrieve games from api

--- lib/services/game_service.dart
+++ lib/services/game_service.dart
@@ -1,6 +1,21 @@
+import 'dart:convert';
+
+import 'package:elite_mobile_app/models/nba/game_info.dart';
+import 'package:http/http.dart' as http;
+
 class GameService {
-  final String _baseUrl = "https://nba-api.afam.app/api/players";
+  final String _baseUrl = "https://nba-api.afam.app/api/games";
   Future<List<GameInfo>> getGames() async {
-    return null;
+    final res = await http.get(Uri.parse(_baseUrl));
+    if (res.statusCode == 200) {
+      Map<String, dynamic> unSerializedData = jsonDecode(res.body);
+      final body = unSerializedData['data'];
+      List<dynamic> rawGames = body['games'];
+      List<GameInfo> games =
+          rawGames.map((dynamic game) => GameInfo.fromJson(game)).toList();
+      return games;
+    } else {
+      throw "Unable to retrieve games";
+    }
   }
 }
