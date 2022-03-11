809b051897d936750f15d6ad0f6ed6e144c3f213
Michael Olie <mike@grolens.com>
2022-03-10 20:52:00 -0800

    added pull to refresh

--- lib/screens/NBASchedule/nba_schedule_page.dart
+++ lib/screens/NBASchedule/nba_schedule_page.dart
@@ -30,61 +30,69 @@ class _NBASchedulePageState extends State<NBASchedulePage> {
                   style: Theme.of(context).textTheme.headline1)),
           Expanded(
               flex: 3,
-              child: FutureBuilder(
-                  future: _gameScheduleFuture,
-                  builder: (BuildContext context,
-                      AsyncSnapshot<List<GameInfo>> snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting ||
-                        !snapshot.hasData) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                    List<GameInfo> _gameSchedule =
-                        snapshot.data as List<GameInfo>;
-                    return SizedBox(
-                        height: 200,
-                        child: ListView.builder(
-                            itemCount: _gameSchedule.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              bool? isRecapAvail =
-                                  _gameSchedule[index].isRecapArticleAvail;
-                              bool? isGameActivated =
-                                  _gameSchedule[index].isGameActivated;
-                              int? currentPeriod =
-                                  _gameSchedule[index].period?.current;
-                              String status = '';
-                              if (isGameActivated == false &&
-                                  currentPeriod == 0) {
-                                status = 'haventStarted';
-                              }
-                              if (isGameActivated == true ||
-                                  currentPeriod! > 0 && currentPeriod < 4) {
-                                status = 'inProgress';
-                              }
-                              if (isRecapAvail == true && currentPeriod == 4) {
-                                status = 'done';
-                              }
-                              return Container(
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 10, horizontal: 0),
-                                  decoration: BoxDecoration(
-                                      border: Border(
-                                          bottom: BorderSide(
-                                              width: 2,
-                                              color:
-                                                  Colors.blueGrey.shade100))),
-                                  child: Row(children: [
-                                    NBAGameTeamInfo(
-                                        teamData: _gameSchedule[index].hTeam,
-                                        isAway: false),
-                                    NBAGameStatus(
-                                        gameStatus: status,
-                                        gameData: _gameSchedule[index]),
-                                    NBAGameTeamInfo(
-                                        teamData: _gameSchedule[index].vTeam,
-                                        isAway: true)
-                                  ]));
-                            }));
-                  }))
+              child: RefreshIndicator(
+                  child: FutureBuilder(
+                      future: _gameScheduleFuture,
+                      builder: (BuildContext context,
+                          AsyncSnapshot<List<GameInfo>> snapshot) {
+                        if (snapshot.connectionState ==
+                                ConnectionState.waiting ||
+                            !snapshot.hasData) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
+                        List<GameInfo> _gameSchedule =
+                            snapshot.data as List<GameInfo>;
+                        return SizedBox(
+                            height: 200,
+                            child: ListView.builder(
+                                physics: const AlwaysScrollableScrollPhysics(),
+                                itemCount: _gameSchedule.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  bool? isRecapAvail =
+                                      _gameSchedule[index].isRecapArticleAvail;
+                                  bool? isGameActivated =
+                                      _gameSchedule[index].isGameActivated;
+                                  int? currentPeriod =
+                                      _gameSchedule[index].period?.current;
+                                  String status = '';
+                                  if (isGameActivated == false &&
+                                      currentPeriod == 0) {
+                                    status = 'haventStarted';
+                                  }
+                                  if (isGameActivated == true ||
+                                      currentPeriod! > 0 && currentPeriod < 4) {
+                                    status = 'inProgress';
+                                  }
+                                  if (isRecapAvail == true &&
+                                      currentPeriod == 4) {
+                                    status = 'done';
+                                  }
+                                  return Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 10, horizontal: 0),
+                                      decoration: BoxDecoration(
+                                          border: Border(
+                                              bottom: BorderSide(
+                                                  width: 2,
+                                                  color: Colors
+                                                      .blueGrey.shade100))),
+                                      child: Row(children: [
+                                        NBAGameTeamInfo(
+                                            teamData:
+                                                _gameSchedule[index].hTeam,
+                                            isAway: false),
+                                        NBAGameStatus(
+                                            gameStatus: status,
+                                            gameData: _gameSchedule[index]),
+                                        NBAGameTeamInfo(
+                                            teamData:
+                                                _gameSchedule[index].vTeam,
+                                            isAway: true)
+                                      ]));
+                                }));
+                      }),
+                  onRefresh: () => gameService.getGames(formatted)))
         ]);
   }
 }
