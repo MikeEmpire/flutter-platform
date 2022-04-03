ecbcdfea59866e64bd2508b12eee1e58968bd398
Michael Olie <mike@grolens.com>
2022-04-03 16:49:48 -0700

    feat: able to show youtube videos smoothly

--- lib/screens/nba_article.dart
+++ lib/screens/nba_article.dart
@@ -1,6 +1,7 @@
 import 'package:elite_mobile_app/models/articles/article_body.dart';
 import 'package:elite_mobile_app/models/articles/elite_article.dart';
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class NBAArticleScreen extends StatelessWidget {
   const NBAArticleScreen({Key? key, required this.article}) : super(key: key);
@@ -9,22 +10,55 @@ class NBAArticleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget _articleBody(List<ArticleBody> articleBody) {
-      return Column(
-          children: articleBody.map((body) {
-        if (body.dataType == 'paragraph') {
-          return Text(body.rawData);
-        }
-        if (body.dataType == 'figure') {
-          return Image.network(body.rawData);
-        }
-        return Text(body.dataType);
-      }).toList());
+      final twitterUrl = article.authorData?.twitter_url as String;
+      final startIndex = twitterUrl.lastIndexOf("/");
+      final lastIndex = twitterUrl.length;
+      final twitterUsername = twitterUrl.substring(startIndex + 1, lastIndex);
+      final String imgUrl = 'https://unavatar.io/twitter/' + twitterUsername;
+      return Column(children: [
+        Row(
+          children: [
+            Text(article.authorData?.displayName as String,
+                style: const TextStyle(color: Colors.black)),
+            Image.network(imgUrl, fit: BoxFit.contain, height: 60, width: 60)
+          ],
+        ),
+        ...articleBody.map((body) {
+          if (body.dataType == 'paragraph') {
+            return Text(body.rawData);
+          }
+          if (body.dataType == 'figure') {
+            return Image.network(body.rawData);
+          }
+          if (body.dataType == 'youtube-video') {
+            final link = body.rawData;
+            final videoId = YoutubePlayer.convertUrlToId(link) as String;
+            return YoutubePlayer(
+              controller: YoutubePlayerController(
+                initialVideoId: videoId,
+                flags: const YoutubePlayerFlags(
+                  hideControls: false,
+                  controlsVisibleAtStart: true,
+                  autoPlay: false,
+                  mute: false,
+                ),
+              ),
+              showVideoProgressIndicator: false,
+              progressIndicatorColor: Colors.blue,
+              progressColors: const ProgressBarColors(
+                  playedColor: Colors.blue, handleColor: Colors.blueAccent),
+            );
+          }
+          return Text(body.dataType);
+        }).toList()
+      ]);
     }
 
     return Scaffold(
         appBar: AppBar(
-          elevation: 0.0,
           backgroundColor: Colors.white,
+          elevation: 0.0,
+          title: Text(article.title),
         ),
         body: Container(
             alignment: const Alignment(0, 0),
@@ -32,9 +66,9 @@ class NBAArticleScreen extends StatelessWidget {
               Hero(
                   tag: 'articleImage',
                   child: Image.network(article.headerImgUrl)),
-              Hero(tag: 'articleTitle', child: Text(article.title)),
               Expanded(
                   child: SingleChildScrollView(
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
                       child: _articleBody(article.flutterBody)))
             ])));
   }
---
+++ lib/widgets/youtube_video.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+class YoutubeVideo extends StatefulWidget {
+  const YoutubeVideo({Key? key, required this.videoUrl}) : super(key: key);
+  final String videoUrl;
+  @override
+  _YoutubeVideoState createState() => _YoutubeVideoState();
+}
+
+class _YoutubeVideoState extends State<YoutubeVideo> {
+  late YoutubePlayerController _controller;
+  late PlayerState _playerState;
+  late YoutubeMetaData _videoMetaData;
+  double _volume = 100;
+  bool _muted = false;
+  bool _isPlayerReady = false;
+  @override
+  void initState() {
+    super.initState();
+
+    void listener() {
+      if (_isPlayerReady && mounted && !_controller.value.isFullScreen) {
+        setState(() {
+          _playerState = _controller.value.playerState;
+          _videoMetaData = _controller.metadata;
+        });
+      }
+    }
+
+    void deactivate() {
+      // Pauses video while navigating to next page.
+      _controller.pause();
+      super.deactivate();
+    }
+
+    @override
+    void dispose() {
+      _controller.dispose();
+      super.dispose();
+    }
+
+    final videoId = YoutubePlayer.convertUrlToId(widget.videoUrl) as String;
+    _controller = YoutubePlayerController(
+      initialVideoId: videoId,
+      flags: const YoutubePlayerFlags(
+        mute: false,
+        autoPlay: true,
+        disableDragSeek: false,
+        loop: false,
+        isLive: false,
+        forceHD: false,
+        enableCaption: true,
+      ),
+    )..addListener(listener);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return YoutubePlayerBuilder(
+      player: YoutubePlayer(
+        controller: _controller,
+        showVideoProgressIndicator: true,
+        progressIndicatorColor: Colors.amberAccent,
+        onReady: () {
+          _isPlayerReady = true;
+        },
+      ),
+      builder: (context, player) => Center(child: player),
+      onExitFullScreen: () {
+        SystemChrome.setPreferredOrientations(DeviceOrientation.values);
+      },
+    );
+  }
+}
